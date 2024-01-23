59bdc02933827263004c022986dc7b449f799ca1
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-01-23 21:58:59 +0530

    rps-ui branch update -- a failure so far, need to properly understand DOM

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 'use strict'
 
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+const ps = document.createElement('p');
+ps.setAttribute('id', 'ps');
+ps.textContent = 0;
+//btn3.appendChild(ps);
+
+const cs = document.createElement('p');
+cs.setAttribute('id', 'cs');
+cs.textContent = 0;
+//ps.appendChild(cs);
+
+const result = document.createElement('div');
+result.setAttribute("id", 'result');
+result.appendChild(ps);
+result.appendChild(cs);
+btn3.appendChild(result);
+
+btn1.addEventListener('click', () =>{
+    playRound('Rock', computerSelection);
+});
+
+btn2.addEventListener('click', () => {
+    playRound('Paper', computerSelection);
+});
+
+btn3.addEventListener('click', () => {
+    playRound('Scissors', computerSelection);
+});
+
+let playerScore = 0;
+let computerScore = 0;
 
 let computerSelection = null;
 let playerSelection = null;
@@ -17,33 +51,39 @@ function getComputerChoice(){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection.toLowerCase() === 'Rock'.toLowerCase() && computerSelection.toLowerCase() === 'Scissor'.toLowerCase()){
+        playerScore = playerScore + 1;
+        //return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === 'Paper'.toLowerCase() && computerSelection.toLowerCase() === 'Rock'.toLowerCase()){
+        playerScore = playerScore + 1;
+        //return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === 'Scissor'.toLowerCase() && computerSelection.toLowerCase() === 'Paper'.toLowerCase()){
+        playerScore = playerScore + 1;
+        //return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        //return "Tie";
+    }
+    else{
+        computerScore = computerScore + 1;
+        //return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    ps.textContent = `Player: ${playerScore}`;
+    cs.textContent = `Computer: ${computerScore}`;
+}
+
 function game(){
 
     computerSelection = getComputerChoice();
     playerSelection = prompt('Input your choice!');
     playRound(playerSelection, computerSelection);
     
-    function playRound(playerSelection, computerSelection){
-        if(playerSelection.toLowerCase() === 'Rock'.toLowerCase() && computerSelection.toLowerCase() === 'Scissor'.toLowerCase()){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-        }
-        else if(playerSelection.toLowerCase() === 'Paper'.toLowerCase() && computerSelection.toLowerCase() === 'Rock'.toLowerCase()){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-        }
-        else if(playerSelection.toLowerCase() === 'Scissor'.toLowerCase() && computerSelection.toLowerCase() === 'Paper'.toLowerCase()){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-        }
-        else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-            return "Tie";
-        }
-        else{
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
-        }
-    }
-    console.log(playRound(playerSelection, computerSelection));
-
-}
-game();
 
 
 //console.log(game(), computerSelection, playerSelection);
+}
