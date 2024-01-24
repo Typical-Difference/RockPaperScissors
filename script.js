28f2cb890c987212bc03030d50220649d92b805a
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-01-24 14:41:50 +0530

    Logic seems to be working, There is no design, need to update with some pretty CSS

--- script.js
+++ script.js
@@ -6,37 +6,40 @@ const btn3 = document.querySelector('#btn3');
 
 const ps = document.createElement('p');
 ps.setAttribute('id', 'ps');
-ps.textContent = 0;
-//btn3.appendChild(ps);
+ps.textContent = 'Player: 0';
 
 const cs = document.createElement('p');
 cs.setAttribute('id', 'cs');
-cs.textContent = 0;
-//ps.appendChild(cs);
+cs.textContent = 'Computer: 0';
 
 const result = document.createElement('div');
 result.setAttribute("id", 'result');
-result.appendChild(ps);
-result.appendChild(cs);
-btn3.appendChild(result);
 
-btn1.addEventListener('click', () =>{
+const endres = document.createElement('div');
+endres.setAttribute("id", "end");
+
+document.body.appendChild(ps);
+document.body.appendChild(cs);
+document.body.appendChild(result);
+document.body.appendChild(endres);
+
+btn1.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
     playRound('Rock', computerSelection);
 });
 
 btn2.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
     playRound('Paper', computerSelection);
 });
 
 btn3.addEventListener('click', () => {
-    playRound('Scissors', computerSelection);
+    let computerSelection = getComputerChoice();
+    playRound('Scissor', computerSelection);
 });
 
 let playerScore = 0;
 let computerScore = 0;
-
-let computerSelection = null;
-let playerSelection = null;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
@@ -52,38 +55,46 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-
-    if(playerSelection.toLowerCase() === 'Rock'.toLowerCase() && computerSelection.toLowerCase() === 'Scissor'.toLowerCase()){
-        playerScore = playerScore + 1;
-        //return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if(playerSelection.toLowerCase() === 'Paper'.toLowerCase() && computerSelection.toLowerCase() === 'Rock'.toLowerCase()){
-        playerScore = playerScore + 1;
-        //return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if(playerSelection.toLowerCase() === 'Scissor'.toLowerCase() && computerSelection.toLowerCase() === 'Paper'.toLowerCase()){
-        playerScore = playerScore + 1;
-        //return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        //return "Tie";
-    }
-    else{
-        computerScore = computerScore + 1;
-        //return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+    
+    endres.textContent = '';
 
     ps.textContent = `Player: ${playerScore}`;
     cs.textContent = `Computer: ${computerScore}`;
-}
 
-function game(){
+    if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissor'){
+        playerScore++;
+        ps.textContent = `Player: ${playerScore}`;
+        result.textContent = `You win! You cast ${playerSelection}, ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock'){
+        playerScore++;
+        ps.textContent = `Player: ${playerScore}`;
+        result.textContent = `You win! You cast ${playerSelection}, ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper'){
+        playerScore++;
+        ps.textContent = `Player: ${playerScore}`;
+        result.textContent = `You win! You cast ${playerSelection}, ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        // It's a tie
+        result.textContent = `It's a tie! You both casted ${playerSelection}`;
+    }
+    else{
+        // User lost
+        computerScore++;
+        cs.textContent = `Computer: ${computerScore}`;
+        result.textContent = `You lost! You cast ${playerSelection}, ${computerSelection} beats ${playerSelection}`;
+    }
 
-    computerSelection = getComputerChoice();
-    playerSelection = prompt('Input your choice!');
-    playRound(playerSelection, computerSelection);
-    
-
-
-//console.log(game(), computerSelection, playerSelection);
+    if(playerScore == 5){
+        endres.textContent = `Yay!! You won ${playerScore} to ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+    if(computerScore == 5){
+        endres.textContent = `Oh oh you lost, try again!!!`;
+        playerScore = 0;
+        computerScore = 0;
+    }
 }
